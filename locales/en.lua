3dd3e469bcf7820bb30b12a0fdccf8e90f473b2e
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-10 16:55:25 -0700

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+    ['entrega_fallida'] = 'You ran out of time for the Delivery!',
+    ['subir_vehiculo'] = 'Get in the Cart',
+    ['carro_incorrecto'] = 'You are not in the right Cart',
+    ['recompensa'] = 'You got $ %s ',
+    ['and'] = ' Gold and ',
+    ['punto_entrega'] = 'Look for the little white flag near %s',
+    ['deliver'] = 'Press [X] to deliver goods',
+    ['blip'] = 'Delivery Point',
+}
